9955cb93c24690d37ce4617e18a168b0227e8705
Anandhan-B <144510508+Anandhan-B@users.noreply.github.com>
2023-12-29 13:03:31 +0530

    ..

---
+++ 02_os.js
@@ -0,0 +1,16 @@
+import os from 'os'
+
+console.log('OS Module');
+
+console.log(os.version()); // Windows 10 Pro Education
+console.log(os.type());    // Windows_NT
+console.log(os.homedir()); // C:\Users\Anandhan
+console.log(os.hostname());
+console.log(os.freemem());
+console.log(os.machine());
+console.log(os.arch());
+//console.log(os.networkInterfaces());
+//console.log(os.userInfo());
+console.log(os.uptime());
+console.log(os.platform());
+console.log(os.release());
